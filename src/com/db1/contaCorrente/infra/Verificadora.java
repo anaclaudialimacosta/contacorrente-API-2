fe5e2a1a513826a47b3a78cae08354cf78c38046
Ana Claudia Lima de Almeida <almeidannynha@gmail.com>
2019-07-26 15:36:38 -0300

    Adicionando trim() na classe Verificadora

--- src/com/db1/contaCorrente/infra/Verificadora.java
+++ src/com/db1/contaCorrente/infra/Verificadora.java
@@ -3,7 +3,7 @@ package com.db1.contaCorrente.infra;
 public class Verificadora {
 	
 	public static void VerificaStringValida(String value, String message) {
-		if(value == null || value.isEmpty()) {
+		if(value == null || value.trim().isEmpty()) {
 			throw new RuntimeException(message);
 		}
 	}
